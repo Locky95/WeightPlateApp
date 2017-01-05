264bd1e2936d6279ab5311557c909234e839a79b
dclocky <dclocky3@gmail.com>
2017-01-05 22:35:49 +0000

    Iterative approach
    iterative approach to the ethod which will decide how many plates should
    be added

---
+++ WeightPlateApp/WeightPlateApp/WeightCalculator.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WeightPlateApp
+{
+    public class WeightCalculator
+    {
+        double weight = 0;
+
+        public WeightCalculator(double weight)
+        {
+            setWeight(weight);
+        }
+
+        public void setWeight(double weight)
+        {
+            this.weight = weight;
+        }
+        public double getWeight()
+        {
+            return weight;
+        }
+
+        public double calcWeight(double weight)
+        {
+            double weightEitherSideOfBar = (weight - 20) / 2;
+            return weightEitherSideOfBar;
+        }
+
+
+        public String weightToDisplay(double weightEitherSideOfBar)
+        {
+            String txt = "";
+            double weightLeft;
+            double weight;
+            txt += "20 plates : " + (int)weightEitherSideOfBar / 20 + "\n";
+            if (weightEitherSideOfBar % 20 > 0)
+            {
+                txt += "20 plates : " + (int)weightEitherSideOfBar / 20 + "\n";
+                weightLeft = (int)weightEitherSideOfBar % 20;
+                if(weightLeft % 20 > 0)
+                {
+                    txt += "15 plates : " + (int)weightLeft / 15 + "\n";
+                    weightLeft = (int)weightLeft % 15;
+                    if(weightLeft%15 > 0)   
+
+                    {
+                        txt += "10 plates : " + (int)weightLeft / 10 + "\n";
+                        weightLeft = (int)weightLeft % 10;
+                        if (weightLeft % 10 > 0)
+                        {
+                            txt += "5 plates : " + (int)weightLeft / 5 + "\n";
+                            weightLeft = weightLeft -  5;
+                            if (weightLeft == 2.5)
+
+                            {
+                                txt += "2.5 plates : 1\n" ;
+                                weightLeft = weightLeft - 2.5;
+                             if (weightLeft >= 1.25 )
+                                {
+                                    txt += "1.25 plates : 1\n";/*+ (int)weightLeft / 1.25 + "\n";*/
+                                }
+                            }
+                        }
+
+                    }
+                }
+            }
+            return txt;
+        }
+
+
+
+        // recursive method
+        public void calcPlatesNeeded(double weight)
+        {
+            subCalcPlatesNeeded(20, calcWeight(100));
+        }
+
+        private String subCalcPlatesNeeded(double plate, double weight)
+        {
+            String txt = "";
+            double weightLeft = 0;
+            txt += plate + " plates : " + (int)weight / plate + "\n";
+            if(weight == 0)
+            {
+                return txt;
+            }
+            if(plate >= 5)
+            {
+                //calc here
+                plate = plate / 2;
+                subCalcPlatesNeeded(plate, weightLeft);
+            }
+            else
+            {
+               
+            }
+            return txt;
+        }
+
+    }
+}
---
+++ WeightPlateApp/WeightPlateTest/UnitTest1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using WeightPlateApp;
+
+
+namespace WeightPlateTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void calcWeightTest()
+        {
+            WeightCalculator a = new WeightCalculator(100);
+
+            double result = a.calcWeight(a.getWeight());
+            double expectedResult = 40;
+
+            Assert.AreEqual(expectedResult, result);
+
+        }
+        [TestMethod]
+        public void displayWeightTest()
+        {
+            WeightCalculator a = new WeightCalculator(40);
+
+            String result = a.weightToDisplay(22.5);
+            String expectedResult = "20 plates : 2 ";
+
+            Assert.AreEqual(expectedResult, result);
+
+        }
+    }
+}
