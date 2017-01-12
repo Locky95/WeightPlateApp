ed15da50247a7037c8534e6aa037c52bb08d1147
dclocky <dclocky3@gmail.com>
2017-01-12 12:39:19 +0000

    Tidied up the Recursive Method
    Tidied up the recursive method for calculating the plates needed for
    either side of the bar.
    Also added a catch for if the user enters a number that is below 20 (the
    weight of the empty bar)

--- WeightPlateApp/WeightPlateApp/WeightCalculator.cs
+++ WeightPlateApp/WeightPlateApp/WeightCalculator.cs
@@ -21,28 +21,24 @@ namespace WeightPlateApp
         {
             return weight;
         }
-        
-
         public double calcWeight(double weight)
         {
             double weightEitherSideOfBar = (weight - 20) / 2;
             return weightEitherSideOfBar;
-        }
-
-           
+        }  
         public String weightToDisplay(double weightEitherSideOfBar)
         {
-            String txt = "Put the following plates on both sides of the bar :                             ";
+            String txt = "Put the following plates on both sides of the bar :                             \n\n";
             double weightLeft;
             weightLeft = calcWeight(weightEitherSideOfBar);
-               
+             if( weightEitherSideOfBar > 20) {   
                txt += "\n20 plates : " + (int) weightLeft / 20 + "     \n";
                 weightLeft = weightLeft % 20;
-                if(weightLeft > 0)
-                { 
+                if (weightLeft > 0)
+                {
                     txt += "\n15 plates : " + (int)weightLeft / 15 + "      \n";
                     weightLeft = weightLeft % 15;
-                    if(weightLeft> 0)   
+                    if (weightLeft > 0)
 
                     {
                         txt += "\n10 plates : " + (int)weightLeft / 10 + "         \n";
@@ -51,61 +47,65 @@ namespace WeightPlateApp
                         {
                             txt += "\n5 plates : " + (int)weightLeft / 5 + "      \n";
                             weightLeft = weightLeft % 5;
-                        if (weightLeft > 0)
+                            if (weightLeft > 0)
                             {
-                                txt += "\n2.5 plates : "+ (int) weightLeft / 2 + "      \n\n" ;
+                                txt += "\n2.5 plates : " + (int)weightLeft / 2 + "      \n";
                                 weightLeft = weightLeft % 2.5;
-                            if (weightLeft >= 1.25 )
+                                if (weightLeft >= 1.25)
                                 {
-                                    txt += "\n1.25 plates : " + (int) weightLeft / 1 + "      \n";
-                                weightLeft = weightLeft % 1.25;
-                                if (weightLeft > 0)
-                                {
-                                    txt += "\nWeight left over = " + (Math.Round(weightLeft, 2)) * 2 ;
-                                }
+                                    txt += "\n1.25 plates : " + (int)weightLeft / 1 + "      \n";
+                                    weightLeft = weightLeft % 1.25;
+                                    if (weightLeft > 0)
+                                    {
+                                        txt += "\nWeight left over = " + (Math.Round(weightLeft, 2)) * 2;
+                                    }
                                 }
                             }
                         }
-
                     }
+                }
+            }
+             else
+            {
+                txt += "The desired weight is less than the bar";
             }
             return txt;
         }
-
-
-
         // recursive method
         public String calcPlatesNeeded(double weight)
         {
-           String txt = subCalcPlatesNeeded(20, calcWeight(weight));
+            String txt;
+            if (weight < 20)
+            {
+                txt = "The desired weight is less than the bar";
+            }
+            else
+            {
+                txt = subCalcPlatesNeeded(25, calcWeight(weight));
+            }
            return txt;
         }
-
         private String subCalcPlatesNeeded(double plate, double weight)
         {
             double weightLeft;
-            String txt = "Total Weight : " + ((weight * 2)+20) + "\n";
-            if (weight <= 1)
+            String txt = "";
+            if (weight == 0)
             {
-                txt += "done";
                 return txt;
             }
             if(plate > 5 && weight > 0)
             {
                 txt += "\n" + plate + "'s : " + (int)weight / (int)plate + "\n";
                 weightLeft = weight % plate;
-                plate = plate - 5;
-                subCalcPlatesNeeded(plate, weightLeft);
+                plate = plate - 5;    
             }
             else
             {
                 txt += "\n" + plate + "'s : " + (int) weight / (int)plate + "\n";
                 weightLeft = weight % plate;
                 plate = plate / 2;
-               subCalcPlatesNeeded(plate, weightLeft);
             }
             return txt += subCalcPlatesNeeded(plate, weightLeft);  
         }
-
     }
 }
